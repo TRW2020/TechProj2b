a6ba9fbe8beea9019712032179da52361c1ea05f
Tyler W <twildrick@usf.edu>
2020-09-17 14:33:46 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ Author: Tyler Wildrick
+ Date: September 17, 2020
+ Comments: This C# Console application code demonstrates the use of 
+           conditional statements after getting input from users
+*/
+
+using System;
+
+namespace TechProj2b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
